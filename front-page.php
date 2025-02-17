c8ca6f4a7c38471dd2a568baf9a62a6855f98b61
AnastasiaAdamoudi <anastasia@pie.co.de>
2025-02-17 18:57:39 +0000

    Created basic hero section for front page

--- front-page.php
+++ front-page.php
@@ -6,7 +6,14 @@ Template Name: Front Page
 get_template_part('template-parts/header');
 ?>
 
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/hero.css">
+
 <main id="primary" class="site-main">
+    <div class="hero">
+        <img src="path/to/your/image.jpg" alt="Hero Image">
+        <div class="hero-text">Text on top of image</div>
+    </div>
+
     <?php
     if (have_posts()) :
         while (have_posts()) :
