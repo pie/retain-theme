a989c287045aff020e2adb17bd56b586cf572f89
AnastasiaAdamoudi <anastasia@pie.co.de>
2025-03-24 15:49:18 +0000

    Styling tweaks

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,16 @@
-module.exports = {
-    plugins: [
-      require("cssnano")({
+export const plugins = [
+    require("cssnano")({
         preset: [
-          "default",
-          {
-            discardComments: { removeAll: true }, // Remove all comments
-            normalizeWhitespace: true, // Minify whitespace
-            minifyParams: true, // Minify parameters
-            colormin: true, // Convert color names to hex
-            minifySelectors: true, // Optimize selectors
-            minifyValues: true, // Convert 0.5 to .5
-          },
+            "default",
+            {
+                discardComments: { removeAll: true }, // Remove all comments
+                normalizeWhitespace: true, // Minify whitespace
+                minifyParams: true, // Minify parameters
+                colormin: true, // Convert color names to hex
+                minifySelectors: true, // Optimize selectors
+                minifyValues: true, // Convert 0.5 to .5
+            },
         ],
-      }),
-    ],
-  };
+    }),
+];
   
